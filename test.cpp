faa19b499e106e0dd0e50ad4255a05286b04b739
atharva-18 <atharvapusalkar18@gmail.com>
2020-04-14 11:43:01 +0530

    Added testing files

--- test.cpp
+++ test.cpp
@@ -1,16 +1,33 @@
 #include <bits/stdc++.h>
+#define MOD 1000000007
+#define INF 2e9
+#define EPS 1e-9
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define endl "\n"
+#define FOR(i,a,b) for (long long i = a; i < b; i++)
+#define REV(i,a,b) for (long long i = a; i >= b; i--)
+#define mset(a,v) memset(a, v, sizeof(a))
+typedef std::pair<int, int> pii;
+typedef std::vector<long long> vi;
+typedef std::vector<vi> vvi;
+typedef long long ll;
+typedef unsigned long long ull;
 using namespace std;
 
+ll mp[1000001];
+ll sum, N;
+
 int main() {
-vector<int> v;
-int i;
-for(i=0;i<100000;i++)
-{
-v.push_back(i);
-}
-for(i=0;i<v.size();i++)
-{
-cout<<v[i]<<endl;
-}
-return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int j = 0;
+    int c=0,fi,se,cnt=0,m=j;
+    cout << fi << " " << se << endl;
+    return 0;
 }
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
-for t in range(int(input())):
-    
+import numpy as np
+
+print(np.empty((2,2), dtype=np.int32))
